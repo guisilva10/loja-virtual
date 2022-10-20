1d0542c9e5cee54b3247a60ba2e1b8dabfe05991
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-10-20 13:16:02 -0300

    style: add responsiveness to payment-confirmation page

--- src/pages/payment-confirmation/payment-confirmation.styles.ts
+++ src/pages/payment-confirmation/payment-confirmation.styles.ts
@@ -6,6 +6,11 @@ display:flex;
 flex-direction:column;
 align-items:center;
 justify-content:center;
+@media(max-width: 768px){
+    width:100%;
+    height:90%;
+  }
+
 `
 
 export const PaymentConfirmationContent = styled.div`
@@ -18,4 +23,9 @@ p{
   font-size:1.125rem;
   font-weight:500;
 }
+
+@media(max-width: 768px){
+    width:100%;
+    padding:30px;
+  }
 `
