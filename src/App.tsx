6fd02835fb62dc94074a0a6b2a3f3e4647e4527a
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-10-20 01:19:53 -0300

    feat: project finally

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import logo from './logo.svg'
-import './App.css'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { useDispatch, useSelector } from 'react-redux'
+import { FunctionComponent, useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
 
-const App = () => {
+// pages
+import HomePage from './pages/home/home.page'
+import LoginPage from './pages/login/login.page'
+import SignUpPage from './pages/sign-up/sign-up.component'
+import ExplorePage from './pages/explore/explore.page'
+import CheckoutPage from './pages/checkout/checkout.page'
+
+// utilities
+import { loginUser, logoutUser } from './store/reducers/user/user.action'
+import { userConverter } from './converters/firestore.converters'
+import { auth, db } from './config/firebase.config'
+
+// Components
+
+import Loading from './components/loading/loading.component'
+import CategoryDetailPage from './pages/category-details/category-details.page'
+import Cart from './components/cart/cart.component'
+import AuthenticationGuard from './guards/authentication.guards'
+import PaymentConfirmationPage from './pages/payment-confirmation/payment-confirmation.page'
+
+const App: FunctionComponent = () => {
+  const [isInitializing, setIsInitializing] = useState(true)
+
+  const dispatch = useDispatch()
+
+  const { isAuthenticated } = useSelector((rootReducer: any) => rootReducer.userReducer)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      const isSignInOut = isAuthenticated && !user
+      if (isSignInOut) {
+        dispatch(logoutUser())
+        return setIsInitializing(false)
+      }
+
+      const isSigningIn = !isAuthenticated && user
+      if (isSigningIn) {
+        const querySnapshot = await getDocs(
+          query(collection(db, 'users').withConverter(userConverter), where('id', '==', user.uid))
+        )
+        const userFromFirestore = querySnapshot.docs[0]?.data()
+
+        dispatch(loginUser(userFromFirestore))
+
+        return setIsInitializing(false)
+      }
+      return setIsInitializing(false)
+    })
+  }, [dispatch])
+
+  if (isInitializing) return <Loading/>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<HomePage/>}/>
+    <Route path='/explore' element={<ExplorePage/>}/>
+    <Route path='/category/:id' element={<CategoryDetailPage/>}/>
+    <Route path='/payment-confirmation' element={<PaymentConfirmationPage/>}/>
+    <Route path='/checkout' element={
+      <AuthenticationGuard>
+        <CheckoutPage/>
+      </AuthenticationGuard>
+    }/>
+    <Route path='/login' element={<LoginPage/>}/>
+    <Route path='/sign-up' element={<SignUpPage/>}/>
+   </Routes>
+   <Cart />
+   </BrowserRouter>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import UserContextProvider from './contexts/user.context'
+import CategoryContextProvider from './contexts/categories.context'
+import CartContextProvider from './contexts/cart.context'
+import store from './store/store'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <UserContextProvider>
+      <CategoryContextProvider>
+        <CartContextProvider>
+        <App />
+        </CartContextProvider>
+      </CategoryContextProvider>
+    </UserContextProvider>
+    </Provider>
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
